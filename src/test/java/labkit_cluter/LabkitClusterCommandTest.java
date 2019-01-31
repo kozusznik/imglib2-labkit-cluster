590609c1768f437c76ad65a20f8ca40cdf4c46ce
Matthias Arzt <arzt@mpi-cbg.de>
2019-01-31 16:41:14 +0100

    Run on Ostrava cluster

--- src/test/java/labkit_cluter/LabkitClusterCommandTest.java
+++ src/test/java/labkit_cluter/LabkitClusterCommandTest.java
@@ -1,6 +1,7 @@
 
 package labkit_cluter;
 
+import cz.it4i.parallel.ImageJServerParadigm;
 import labkit_cluster.JsonIntervals;
 import labkit_cluster.LabkitClusterCommand;
 import labkit_cluster.MyN5;
@@ -12,9 +13,10 @@ import net.imglib2.util.Intervals;
 import org.scijava.Context;
 import org.scijava.parallel.ParallelService;
 import org.scijava.parallel.ParallelizationParadigm;
-import org.scijava.parallel.utils.StartImageJServer;
+import org.scijava.parallel.ParallelizationParadigmProfile;
 
 import java.util.AbstractList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +26,12 @@ import java.util.stream.Collectors;
 public class LabkitClusterCommandTest {
 
 	public final static String OUTPUT_N5_DIRECTORY =
-		"/home/arzt/tmp/output/result.xml";
+		"/home/training/dd-18-37-29/output";
+	final String CLASSIFIER_PATH =
+		"/home/training/dd-18-37-29/input/classifier.classifier";
+	private final String inputXml = "/home/training/dd-18-37-29/input/export.xml";
+	private static List<String> hosts = Arrays.asList("localhost:10001",
+		"localhost:10002", "localhost:10003", "localhost:10004");
 
 	private Context context = new Context();
 	private ParallelService parallelService = context.service(
@@ -39,29 +46,37 @@ public class LabkitClusterCommandTest {
 	public void startServerAndRun() throws ExecutionException,
 		InterruptedException
 	{
-		Process process = StartImageJServer.startImageJServerIfNecessary(
-			"/home/arzt/Applications/Fiji.app/");
-		try (ParallelizationParadigm paradigm = StartImageJServer.getTestParadigm(
-			parallelService))
-		{
+		// Process process = StartImageJServer.startImageJServerIfNecessary(
+		// "/home/arzt/Applications/Fiji.app/");
+		try (ParallelizationParadigm paradigm = getTestParadigm(parallelService)) {
 			run(paradigm);
 		}
 		finally {
-			if (process != null) process.destroy();
+			// if (process != null) process.destroy();
 		}
 	}
 
+	public static ParallelizationParadigm getTestParadigm(
+		ParallelService parallelService)
+	{
+		parallelService.deleteProfiles();
+		parallelService.addProfile(new ParallelizationParadigmProfile(
+			ImageJServerParadigm.class, "lonelyBiologist01"));
+		parallelService.selectProfile("lonelyBiologist01");
+		ParallelizationParadigm paradigm = parallelService.getParadigm();
+		((ImageJServerParadigm) paradigm).setHosts(hosts);
+		paradigm.init();
+		return paradigm;
+	}
+
 	private void run(ParallelizationParadigm paradigm) {
-		final String inputXml =
-			"/home/arzt/Documents/Datasets/Mouse Brain/hdf5/export.xml";
-		final String value =
-			"/home/arzt/Documents/Datasets/Mouse Brain/hdf5/classifier.classifier";
 		final long[] dimensions = Intervals.dimensionsAsLongArray(
-			new SpimDataInputImage(inputXml, 0).interval());
+			new SpimDataInputImage("/home/arzt/salomon/input/export.xml", 0)
+				.interval());
 		CellGrid grid = new CellGrid(dimensions, new int[] { 100, 100, 100 });
-		MyN5.createDataset(OUTPUT_N5_DIRECTORY, grid);
+		MyN5.createDataset("/home/arzt/salomon/output", grid);
 		List<Map<String, ?>> parameters = initializeParameters(OUTPUT_N5_DIRECTORY,
-			inputXml, value, grid);
+			inputXml, CLASSIFIER_PATH, grid);
 		paradigm.runAll(nCopies(LabkitClusterCommand.class, parameters.size()),
 			parameters);
 		System.out.println("Results written to: " + OUTPUT_N5_DIRECTORY);
--- src/test/java/labkit_cluter/ShowResult.java
+++ src/test/java/labkit_cluter/ShowResult.java
@@ -2,6 +2,7 @@
 package labkit_cluter;
 
 import bdv.util.BdvFunctions;
+import bdv.util.volatiles.VolatileViews;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import org.janelia.saalfeldlab.n5.N5FSReader;
@@ -12,10 +13,10 @@ import java.io.IOException;
 public class ShowResult {
 
 	public static void main(String... args) throws IOException {
-		N5FSReader reader = new N5FSReader(
-			LabkitClusterCommandTest.OUTPUT_N5_DIRECTORY);
+		N5FSReader reader = new N5FSReader("/home/arzt/salomon/output");
 		RandomAccessibleInterval<UnsignedByteType> result = N5Utils
 			.openWithDiskCache(reader, "segmentation", new UnsignedByteType());
-		BdvFunctions.show(result, "N5!!").setDisplayRange(0, 1);
+		BdvFunctions.show(VolatileViews.wrapAsVolatile(result), "N5!!")
+			.setDisplayRange(0, 1);
 	}
 }

286bed5e2c96689f44d2f0c752064e803745026a
Jan Kožusznik <jan@kozusznik.cz>
2019-05-17 14:42:35 +0200

    code: Organize imports.

--- src/main/java/labkit_cluster/interactive/SegmentCommand.java
+++ src/main/java/labkit_cluster/interactive/SegmentCommand.java
@@ -4,19 +4,20 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
-import labkit_cluster.headless.JsonIntervals;
-import net.imagej.Dataset;
 import net.imglib2.Interval;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.labkit.inputimage.SpimDataInputImage;
 import net.imglib2.labkit.segmentation.weka.TrainableSegmentationSegmenter;
 import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
+
 import org.scijava.Context;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+
+import labkit_cluster.headless.JsonIntervals;
 
 @Plugin( type = Command.class )
 public class SegmentCommand implements Command
